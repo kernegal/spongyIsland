44bab4548f93a657ffda8c5f7742390a2a7b4d89
kernegal <legan.xtv@gmail.com>
2016-10-03 00:06:10 +0200

    Added to repository

---
+++ src/main/java/io/github/kernegal/spongyisland/SpongyIsland.java
@@ -0,0 +1,11 @@
+package io.github.kernegal.spongyisland;
+
+import org.spongepowered.api.plugin.Plugin;
+
+/**
+ * Created by kernegal on 02/10/2016.
+ * Base class for SpongyIsland plugin
+ */
+@Plugin(id = "SpongyIsland", name = "Spongy Island", version = "0.1")
+public class SpongyIsland {
+}
